ddbc393b7ef63c6022a08700765c97ccabd34c93
holazz <2418184580@qq.com>
2022-09-23 14:12:52 +0800

    feat: exclude react config from default package

--- packages/all/index.js
+++ packages/all/index.js
@@ -2,9 +2,5 @@
 const { defineConfig } = require('eslint-define-config')
 
 module.exports = defineConfig({
-  extends: [
-    '@holazz/eslint-config-react',
-    '@holazz/eslint-config-vue',
-    '@holazz/eslint-config-prettier',
-  ],
+  extends: ['@holazz/eslint-config-vue', '@holazz/eslint-config-prettier'],
 })
